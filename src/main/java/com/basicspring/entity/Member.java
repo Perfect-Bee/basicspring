60bfe98dc6635d12308a660ccecce67170826788
perfect-Bee <joonslee3@gmail.com>
2025-12-30 16:33:20 +0900

    name 생성자 설정

--- src/main/java/com/basicspring/entity/Member.java
+++ src/main/java/com/basicspring/entity/Member.java
@@ -13,4 +13,9 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+    public Member(String name) {
+        this.name = name;
+    }
+
 }
